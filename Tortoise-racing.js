fb3dc3c5e9c7d748721e6f109d74bced69caff42
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-08 09:46:29 -0800

    Update Tortoise-racing.js

--- Tortoise-racing.js
+++ Tortoise-racing.js
@@ -29,3 +29,8 @@ Hints for people who don't know how to convert to hours, minutes, seconds:
 
 
 // Solution
+
+function race(v1, v2, g) {
+  let time = g/(v2-v1);
+  return v2-v1 ? [Math.floor(time),Math.floor(time*60%60),Math.floor(time*3600%60)] : null; 
+}
